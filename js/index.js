3e7f838f3e48a98d3d9217a7f964510413989537
Asharry97 <Asharry97@users.noreply.github.com>
2020-09-13 20:49:28 +0530

    added footer and navbar.html & footer.html component dividing

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// function load_footer() {
+//     document.queryselector("div.footer-component").innerhtml = '<object type="text/html" data="footer.html" ></object>';
+// }
+
+async function load_navbar() {
+    document.querySelector("div.navbar-component").innerHTML = await (await fetch('navbar.html')).text();
+}
+
+async function load_footer() {
+    document.querySelector("div.footer-component").innerHTML = await (await fetch('footer.html')).text();
+}
+
+load_navbar();
+load_footer();
